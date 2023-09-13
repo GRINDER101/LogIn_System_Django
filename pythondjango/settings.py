39d98d4eada61cc4fb87f8249d533fd7079f6ffa
Rittik Saha <rittiksaha7044@gmail.com>
2023-09-13 21:16:25 +0100

    files committed

--- pythondjango/settings.py
+++ pythondjango/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = "pythondjango.wsgi.application"
 
 DATABASES ={
     "default": {
-        #"ENGINE" : "django.db.backends.sqlite3",
+        "ENGINE" : "django.db.backends.postgresql",
         "NAME" : "railway",
         "USER" : "postgres",
         "PASSWORD" : "zSpJ9tOTMjv300QHbat7",
