abc3427e8cf6baaad13d640d727c242bb5aca2f5
Rittik Saha <rittiksaha7044@gmail.com>
2023-09-13 07:46:04 +0100

    files committed

--- pythondjango/settings.py
+++ pythondjango/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from . info import *
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASE_URL = "PGPASSWORD=zSpJ9tOTMjv300QHbat7 psql -h containers-us-west-139.railway.app -U postgres -p 7835 -d railway"
 
 EMAIL_BACKEND = EMAIL_BACKEND
 EMAIL_USE_TLS = EMAIL_USE_TLS
@@ -83,7 +86,7 @@ WSGI_APPLICATION = "pythondjango.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# import dj_database_url
+
 
 # DATABASES = {
 #     "default": {
@@ -92,17 +95,20 @@ WSGI_APPLICATION = "pythondjango.wsgi.application"
 #     }
 # }
 
-DATABASES ={
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME" : "railway",
-        "USER" : "postgres",
-        "PASSWORD" : "zSpJ9tOTMjv300QHbat7",
-        "HOST" : "containers-us-west-139.railway.app",
-        "PORT" : "7835",
-    }
-}
+# DATABASES ={
+#     "default": {
+#         "ENGINE" : "django.db.backends.sqlite3",
+#         "NAME" : "railway",
+#         "USER" : "postgres",
+#         "PASSWORD" : "zSpJ9tOTMjv300QHbat7",
+#         "HOST" : "containers-us-west-139.railway.app",
+#         "PORT" : "7835",
+#     }
+# }
 
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # DATABASES['default']['OPTIONS']['charset'] = 'utf8mb4'
