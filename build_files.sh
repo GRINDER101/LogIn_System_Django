cbd27d0c0e40cef198ebaa298d5c87ae431c7b0a
Rittik Saha <rittiksaha7044@gmail.com>
2023-09-13 01:13:55 +0100

    files committed

--- build_files.sh
+++ build_files.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-python2.7 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python2.7 manage.py makemigrations --noinput
-python2.7 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collect Static..."
-python2.7 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
